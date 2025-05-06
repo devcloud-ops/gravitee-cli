a61bdb727f89addef119d1ba7b708cefd0568362
Muhammad Faisal Siddiqui <61173238+devcloud-ops@users.noreply.github.com>
2025-05-06 12:48:47 +1000

    Update __init__.py
    Path object is directly passed to pkgutil.iter_modules() which is causing AttributeError: 'PosixPath' object has no attribute 'startswith'

--- graviteeio_cli/commands/apim/apis/plugins/__init__.py
+++ graviteeio_cli/commands/apim/apis/plugins/__init__.py
@@ -3,5 +3,5 @@ import pkgutil
 
 COMMANDS = []
 
-for (_, name, _) in pkgutil.iter_modules([Path(__file__).parent]):
+for (_, name, _) in pkgutil.iter_modules([str(Path(__file__).parent)]):
     COMMANDS.append(name[4:len(name)])
